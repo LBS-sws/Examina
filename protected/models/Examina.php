d94126c9d5006eff79cffe1b64da7b7ebc10f036
shenchao <shenchao@lbsgroup.com.cn>
2021-08-26 10:28:32 +0800

    重寫試題的隨機規則

--- protected/models/Examina.php
+++ protected/models/Examina.php
@@ -2,6 +2,7 @@
 class Examina{
 
     public $_quizId;//測驗單id
+    public $_joinId;
     //public $_typeId;//類別id (後期刪除)
     public $_bool;//是否隨機出試題 true:是
 
@@ -9,6 +10,7 @@ class Examina{
 
     public $_testList=array();//題庫列表
     public $_errorList=array();//員工錯誤列表
+    public $_successList=array();//員工正確列表
     public $_resultList=array();//輸出列表
 
     public $_quizList=array();//測驗單信息
@@ -17,6 +19,7 @@ class Examina{
     private $_errorBool = false;//是否有異常 true：數據異常
 
     public function __construct($index,$bool=true,$join_id=""){
+        $this->_joinId = $join_id;
         $this->_quizId = $index;
         $this->_bool = $bool;
         $staff_id = Yii::app()->user->staff_id();//當前員工
@@ -25,7 +28,12 @@ class Examina{
             return true;
         }
         $command = Yii::app()->db->createCommand();
-        //$this->_command->reset();
+        if(empty($this->_joinId)){//最後一次參與測試的id
+            $this->_joinId = $command->select("max(id)")->from("exa_join")
+                ->where("employee_id=:id and quiz_id=:quiz_id", array(':id'=>$staff_id,':quiz_id'=>$index))->queryScalar();
+            $this->_joinId = $this->_joinId?$this->_joinId:"";
+            $command->reset();
+        }
         $rows = $command->select()->from("exa_quiz")
             ->where("id=:id", array(':id'=>$index))->queryRow();
         if($rows){
@@ -73,14 +81,21 @@ class Examina{
             }
             $command->reset();
 
-            //由於測驗單只能進行一次，且試題為單個綁定測驗單，所以試題錯誤列表只能為模擬測驗提供
-            $rows = $command->select("a.title_id")->from("exa_examina a")
+            //最後一次測驗單的試題
+            $exprSql = empty($this->_joinId)?"":" and a.join_id='{$this->_joinId}'";
+            $rows = $command->select("a.title_id,b.judge")->from("exa_examina a")
                 ->leftJoin("exa_title_choose b","a.choose_id=b.id")
                 ->leftJoin("exa_title c","a.title_id=c.id")
-                ->where("a.employee_id=:employee_id and b.judge=0 and c.quiz_id=:quiz_id",
+                ->where("a.employee_id=:employee_id and c.quiz_id=:quiz_id $exprSql",
                     array(':employee_id'=>$staff_id,':quiz_id'=>$this->_quizId))->queryAll();
             if($rows){
-                $this->_errorList = array_column($rows,"title_id","title_id");
+                foreach ($rows as $row){
+                    if($row["judge"]==1){
+                        $this->_successList[$row["title_id"]] = $row["title_id"];
+                    }else{
+                        $this->_errorList[$row["title_id"]] = $row["title_id"];
+                    }
+                }
             }
 
             //$this->_resultList =  array_merge(array_column($this->_testList,"id"),$this->_errorList);
@@ -90,21 +105,46 @@ class Examina{
         }
     }
 
-    private function setResultList(&$command){
+    //錯誤試題百分百出現，然後選擇沒參與過的試題，最後從成功試題中隨機
+    private function getRandListForKey(){
         $testList = array_column($this->_testList,"id","id");
         $errorList = $this->_errorList;
+        $successList = $this->_successList;
+        if(count($errorList)>=$this->_testNum){
+            $list = array_rand($errorList,$this->_testNum);
+            $list = $this->_testNum==1?array($list):$list;
+        }else{
+            $list = empty($errorList)?array():array_values($errorList);
+            $exprList = array();//上次測驗沒抽中的試題
+            foreach ($testList as $item){
+                if(!in_array($item,$errorList)&&!in_array($item,$successList)){
+                    $exprList[$item] = $item;
+                }
+            }
+            $num = $this->_testNum-count($errorList);
+            if($num<=count($exprList)){
+                $exprList = array_rand($exprList,$num);
+                $exprList = $num==1?array($exprList):$exprList;
+                $list = array_merge($list,$exprList);
+            }else{
+                $num-=count($exprList);
+                $list = array_merge($list,$exprList);//補上沒抽中試題
+                $exprList = array_rand($successList,$num);//從成功試題中隨機最後幾個試題
+                $exprList = $num==1?array($exprList):$exprList;
+                $list = array_merge($list,$exprList);
+            }
+        }
+        shuffle($list);
+        return $list;
+    }
+
+    private function setResultList(&$command){
         $resultList = array();
         $sessionList = array();
         $session = Yii::app()->session;
+        $list = $this->getRandListForKey();
 
-        for ($i=0;$i<$this->_testNum;$i++){
-            $list = array_merge($testList,$errorList);
-            $randNum = array_rand($list,1);
-            $randNum = $list[$randNum];
-            unset($testList[$randNum]);
-            if(key_exists($randNum,$errorList)){
-                unset($errorList[$randNum]);
-            }
+        foreach ($list as $randNum){
             //$randNum = intval($randNum);
             $result = $this->_testList[$randNum];
             $result["list"]=$this->getRandChooseList($command,$result["id"]);
