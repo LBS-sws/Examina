3b3922b97131cdc9d3a5096aab653c9bf6a1d513
shenchao <shenchao@lbsgroup.com.cn>
2025-12-01 15:02:19 +0800

    从服务器获取代码重新提交到GitHub

--- protected/config/menu.php
+++ protected/config/menu.php
@@ -1,84 +1,356 @@
-<?php
-
-return array(
-    'examina'=>array(
-        'access'=>'EM',
-		'icon'=>'fa-pencil',
-        'items'=>array(
-            'My test'=>array(
-                'access'=>'EM01',
-                'url'=>'/myTest/index',
-            ),
-            'Simulation test'=>array(
-                'access'=>'EM02',
-                'url'=>'/simTest/index',
-            ),
-        ),
+<?php return array (
+  '企业文化' => 
+  array (
+    'access' => 'QYWH',
+    'icon' => 'fa-pencil',
+    'items' => 
+    array (
+      'Study guide' => 
+      array (
+        'access' => 'QYWH01',
+        'url' => '/StudyClass/index?index=2&menu_code=QYWH',
+      ),
+      'Mock test' => 
+      array (
+        'access' => 'QYWH02',
+        'url' => '/MockChapter/index?index=2&menu_code=QYWH',
+      ),
+      'Markedly test' => 
+      array (
+        'access' => 'QYWH07',
+        'url' => '/MarkedlyTest/index?index=2&menu_code=QYWH',
+      ),
+      'Study mutual' => 
+      array (
+        'access' => 'QYWH03',
+        'url' => '/Mutual/index?index=2&menu_code=QYWH',
+      ),
+      'Study mutual audit' => 
+      array (
+        'access' => 'QYWH10',
+        'url' => '/MutualAudit/index?index=2&menu_code=QYWH',
+      ),
+      'Paper Wrong' => 
+      array (
+        'access' => 'QYWH04',
+        'url' => '/PaperWrong/index?index=2&menu_code=QYWH',
+      ),
+      'Paper My' => 
+      array (
+        'access' => 'QYWH05',
+        'url' => '/PaperMy/index?index=2&menu_code=QYWH',
+      ),
+      'Conclude Paper' => 
+      array (
+        'access' => 'QYWH06',
+        'url' => '/concludePaper/index?index=2&menu_code=QYWH',
+      ),
+      'Conclude Staff' => 
+      array (
+        'access' => 'QYWH08',
+        'url' => '/concludeStaff/index?index=2&menu_code=QYWH',
+      ),
+      'Conclude Question' => 
+      array (
+        'access' => 'QYWH09',
+        'url' => '/concludeQuestion/index?index=2&menu_code=QYWH',
+      ),
+      'Video link hits' => 
+      array (
+        'access' => 'QYWH11',
+        'url' => '/videoHits/index?index=2&menu_code=QYWH',
+      ),
     ),
-    'System Setting'=>array(
-        'access'=>'SS',
-		'icon'=>'fa-gear',
-        'items'=>array(
-            'Test list'=>array(
-                'access'=>'SS02',
-                'url'=>'/testTop/index',
-            ),
-            'menu setting'=>array(
-                'access'=>'SS03',
-                'url'=>'/menuSet/index',
-            ),
-        ),
+  ),
+  '销售部' => 
+  array (
+    'access' => 'XS',
+    'icon' => 'fa-pencil',
+    'items' => 
+    array (
+      'Study guide' => 
+      array (
+        'access' => 'XS01',
+        'url' => '/StudyClass/index?index=4&menu_code=XS',
+      ),
+      'Mock test' => 
+      array (
+        'access' => 'XS02',
+        'url' => '/MockChapter/index?index=4&menu_code=XS',
+      ),
+      'Markedly test' => 
+      array (
+        'access' => 'XS07',
+        'url' => '/MarkedlyTest/index?index=4&menu_code=XS',
+      ),
+      'Study mutual' => 
+      array (
+        'access' => 'XS03',
+        'url' => '/Mutual/index?index=4&menu_code=XS',
+      ),
+      'Study mutual audit' => 
+      array (
+        'access' => 'XS10',
+        'url' => '/MutualAudit/index?index=4&menu_code=XS',
+      ),
+      'Paper Wrong' => 
+      array (
+        'access' => 'XS04',
+        'url' => '/PaperWrong/index?index=4&menu_code=XS',
+      ),
+      'Paper My' => 
+      array (
+        'access' => 'XS05',
+        'url' => '/PaperMy/index?index=4&menu_code=XS',
+      ),
+      'Conclude Paper' => 
+      array (
+        'access' => 'XS06',
+        'url' => '/concludePaper/index?index=4&menu_code=XS',
+      ),
+      'Conclude Staff' => 
+      array (
+        'access' => 'XS08',
+        'url' => '/concludeStaff/index?index=4&menu_code=XS',
+      ),
+      'Conclude Question' => 
+      array (
+        'access' => 'XS09',
+        'url' => '/concludeQuestion/index?index=4&menu_code=XS',
+      ),
+      'Video link hits' => 
+      array (
+        'access' => 'XS11',
+        'url' => '/videoHits/index?index=4&menu_code=XS',
+      ),
     ),
-    'statistical'=>array(
-        'access'=>'SC',
-		'icon'=>'fa-bar-chart',
-        'items'=>array(
-            'Test results statistics'=>array(
-                'access'=>'SC01',
-                'url'=>'/statisticsTest/index',
-            ),
-            'Title results statistics'=>array(
-                'access'=>'SC02',
-                'url'=>'/statisticsTitle/index',
-            ),
-            'Staff results statistics'=>array(
-                'access'=>'SC03',
-                'url'=>'/statisticsStaff/index',
-            ),
-            'Quiz results statistics'=>array(
-                'access'=>'SC04',
-                'url'=>'/statisticsQuiz/index',
-            ),
-        ),
+  ),
+  '管理层' => 
+  array (
+    'access' => 'GLC',
+    'icon' => 'fa-pencil',
+    'items' => 
+    array (
+      'Study guide' => 
+      array (
+        'access' => 'GLC01',
+        'url' => '/StudyClass/index?index=3&menu_code=GLC',
+      ),
+      'Mock test' => 
+      array (
+        'access' => 'GLC02',
+        'url' => '/MockChapter/index?index=3&menu_code=GLC',
+      ),
+      'Markedly test' => 
+      array (
+        'access' => 'GLC07',
+        'url' => '/MarkedlyTest/index?index=3&menu_code=GLC',
+      ),
+      'Study mutual' => 
+      array (
+        'access' => 'GLC03',
+        'url' => '/Mutual/index?index=3&menu_code=GLC',
+      ),
+      'Study mutual audit' => 
+      array (
+        'access' => 'GLC10',
+        'url' => '/MutualAudit/index?index=3&menu_code=GLC',
+      ),
+      'Paper Wrong' => 
+      array (
+        'access' => 'GLC04',
+        'url' => '/PaperWrong/index?index=3&menu_code=GLC',
+      ),
+      'Paper My' => 
+      array (
+        'access' => 'GLC05',
+        'url' => '/PaperMy/index?index=3&menu_code=GLC',
+      ),
+      'Conclude Paper' => 
+      array (
+        'access' => 'GLC06',
+        'url' => '/concludePaper/index?index=3&menu_code=GLC',
+      ),
+      'Conclude Staff' => 
+      array (
+        'access' => 'GLC08',
+        'url' => '/concludeStaff/index?index=3&menu_code=GLC',
+      ),
+      'Conclude Question' => 
+      array (
+        'access' => 'GLC09',
+        'url' => '/concludeQuestion/index?index=3&menu_code=GLC',
+      ),
+      'Video link hits' => 
+      array (
+        'access' => 'GLC11',
+        'url' => '/videoHits/index?index=3&menu_code=GLC',
+      ),
     ),
-    'training process'=>array(
-        'access'=>'TP',
-		'icon'=>'fa-superpowers',
-        'items'=>array(
-            'Enterprise situation'=>array(
-                'access'=>'TP01',
-                'url'=>'/enterprise/index',
-            ),
-            'Operation study'=>array(
-                'access'=>'TP02',
-                'url'=>'/practice/index',
-            ),
-            'Theoretical knowledge'=>array(
-                'access'=>'TP03',
-                'url'=>'/Theory/index',
-            ),
-            'Communication answer'=>array(
-                'access'=>'TP04',
-                'url'=>'/answer/index',
-            ),
-            'exam(Theory + practice)'=>array(
-                'access'=>'TP05',
-                'url'=>'/examTheory/index',
-            ),
-            'System exam(In previous)'=>array(
-                'access'=>'TP06',
-                'url'=>'/examPrevious/index',
-            ),
-        ),
+  ),
+  '技术部' => 
+  array (
+    'access' => 'JS',
+    'icon' => 'fa-pencil',
+    'items' => 
+    array (
+      'Study guide' => 
+      array (
+        'access' => 'JS01',
+        'url' => '/StudyClass/index?index=1&menu_code=JS',
+      ),
+      'Mock test' => 
+      array (
+        'access' => 'JS02',
+        'url' => '/MockChapter/index?index=1&menu_code=JS',
+      ),
+      'Markedly test' => 
+      array (
+        'access' => 'JS07',
+        'url' => '/MarkedlyTest/index?index=1&menu_code=JS',
+      ),
+      'Study mutual' => 
+      array (
+        'access' => 'JS03',
+        'url' => '/Mutual/index?index=1&menu_code=JS',
+      ),
+      'Study mutual audit' => 
+      array (
+        'access' => 'JS10',
+        'url' => '/MutualAudit/index?index=1&menu_code=JS',
+      ),
+      'Paper Wrong' => 
+      array (
+        'access' => 'JS04',
+        'url' => '/PaperWrong/index?index=1&menu_code=JS',
+      ),
+      'Paper My' => 
+      array (
+        'access' => 'JS05',
+        'url' => '/PaperMy/index?index=1&menu_code=JS',
+      ),
+      'Conclude Paper' => 
+      array (
+        'access' => 'JS06',
+        'url' => '/concludePaper/index?index=1&menu_code=JS',
+      ),
+      'Conclude Staff' => 
+      array (
+        'access' => 'JS08',
+        'url' => '/concludeStaff/index?index=1&menu_code=JS',
+      ),
+      'Conclude Question' => 
+      array (
+        'access' => 'JS09',
+        'url' => '/concludeQuestion/index?index=1&menu_code=JS',
+      ),
+      'Video link hits' => 
+      array (
+        'access' => 'JS11',
+        'url' => '/videoHits/index?index=1&menu_code=JS',
+      ),
     ),
+  ),
+  'examina' => 
+  array (
+    'access' => 'EM',
+    'icon' => 'fa-pencil',
+    'items' => 
+    array (
+      'My test' => 
+      array (
+        'access' => 'EM01',
+        'url' => '/myTest/index',
+      ),
+      'Simulation test' => 
+      array (
+        'access' => 'EM02',
+        'url' => '/simTest/index',
+      ),
+    ),
+  ),
+  'System Setting' => 
+  array (
+    'access' => 'SS',
+    'icon' => 'fa-gear',
+    'items' => 
+    array (
+      'Test list' => 
+      array (
+        'access' => 'SS02',
+        'url' => '/testTop/index',
+      ),
+      'menu setting' => 
+      array (
+        'access' => 'SS03',
+        'url' => '/menuSet/index',
+      ),
+    ),
+  ),
+  'statistical' => 
+  array (
+    'access' => 'SC',
+    'icon' => 'fa-bar-chart',
+    'items' => 
+    array (
+      'Test results statistics' => 
+      array (
+        'access' => 'SC01',
+        'url' => '/statisticsTest/index',
+      ),
+      'Title results statistics' => 
+      array (
+        'access' => 'SC02',
+        'url' => '/statisticsTitle/index',
+      ),
+      'Staff results statistics' => 
+      array (
+        'access' => 'SC03',
+        'url' => '/statisticsStaff/index',
+      ),
+      'Quiz results statistics' => 
+      array (
+        'access' => 'SC04',
+        'url' => '/statisticsQuiz/index',
+      ),
+    ),
+  ),
+  'training process' => 
+  array (
+    'access' => 'TP',
+    'icon' => 'fa-superpowers',
+    'items' => 
+    array (
+      'Enterprise situation' => 
+      array (
+        'access' => 'TP01',
+        'url' => '/enterprise/index',
+      ),
+      'Operation study' => 
+      array (
+        'access' => 'TP02',
+        'url' => '/practice/index',
+      ),
+      'Theoretical knowledge' => 
+      array (
+        'access' => 'TP03',
+        'url' => '/Theory/index',
+      ),
+      'Communication answer' => 
+      array (
+        'access' => 'TP04',
+        'url' => '/answer/index',
+      ),
+      'exam(Theory + practice)' => 
+      array (
+        'access' => 'TP05',
+        'url' => '/examTheory/index',
+      ),
+      'System exam(In previous)' => 
+      array (
+        'access' => 'TP06',
+        'url' => '/examPrevious/index',
+      ),
+    ),
+  ),
 );
